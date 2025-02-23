753f01337939e5e0c9cfd17527a9f6ccf83570ea
Shubham Rade <ssrade2005@gmail.com>
2025-02-23 16:21:26 +0530

    Repo Initialised

---
+++ herbiverse/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Home from './Components/Home/Home.jsx'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'element={<Layout />}>
+    <Route path=''element={<Home/>}></Route>
+    
+
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
